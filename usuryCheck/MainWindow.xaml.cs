a269f050d21172b624e972c2b4ae9416e1e57da0
Nickleus Jimenez <Nickleus-J@live.com.ph>
2025-12-06 21:53:43 +0800

    Theming

--- usuryCheck/MainWindow.xaml.cs
+++ usuryCheck/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-
+using MahApps.Metro.Controls;
 
 namespace usuryCheck
 {
